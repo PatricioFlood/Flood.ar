7741deb2ee03c0c3af8d2fa7081daeda0e5ac56b
Patricio <pflood@frba.utn.edu.ar>
2022-04-15 13:25:20 -0300

    landing page

---
+++ public/scripts.js
@@ -0,0 +1,45 @@
+var textWrapper = document.querySelector('.title');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var textWrapper2 = document.querySelector('.text');
+textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline()
+.add({
+    targets: '.title',
+    opacity: 1,
+    easing: "linear",
+    duration: 1,
+  })
+  .add({
+    targets: '.title .letter',
+    scale: [2,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 1000,
+    delay: (el, i) => 300*i
+  })
+ 
+anime.timeline()
+  .add({
+    duration: 2500
+  })
+  .add({
+    targets: '.text',
+    opacity: 1,
+    easing: "linear",
+    duration: 1,
+  })
+  .add({
+    targets: '.text .letter',
+    translateY: [-50,0],
+    easing: "linear",
+    duration: 300,
+    delay: (el, i) => 50 * i
+  })
+  .add({
+    targets: '.underline',
+    opacity: 1,
+    easing: "linear",
+    duration: 200
+  })
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Flood.ar
+
+Landing page of domain ['flood.ar'](https://flood.ar)
